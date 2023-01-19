1fd5bc564eff26530084d5c893a48849c6217e24
Boxer <aayushbajaj7@gmail.com>
2023-01-19 23:58:43 +0000

    correct representation

--- int_to_bits.c
+++ int_to_bits.c
@@ -3,9 +3,10 @@
 
 char *int_to_bits(int i)
 {
-    char *bit_string = malloc(sizeof(int) * 8);
+    int n = sizeof(int) * 8;
+    char *bit_string = malloc(n--); // reduces value of n for the subsequent shift
 
-    for (int n = sizeof(i) * 8, j = 0; n > 0, j < 32; n--, j++) {
+    for (int j = 0;n >= 0;n--, j++) {
         bit_string[j] = '0' + ((i >> n) & 1);
     }
 
